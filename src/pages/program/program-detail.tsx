8def3e5b93efb23ee90ff265979c981a37f5ab5b
siyoon <siyoon@friendli.ai>
2023-09-17 19:48:53 +0900

    add programs

--- src/pages/program/program-detail.tsx
+++ src/pages/program/program-detail.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
+import type { ProgramsJSONProps } from "./program";
+import ProgramsJSON from "./programs-23.json";
+import classes from "./programs-detail.module.scss";
+
+const { programs } = JSON.parse(
+  JSON.stringify(ProgramsJSON)
+) as ProgramsJSONProps;
+
 function ProgramDetailPage() {
   const { programName = "" } = useParams();
+
+  const data = programs.find((p) => String(p.id) === programName);
+
+  if (!data) return null;
+
   return (
-    <div>
-      <Link to="..">back</Link>program detail param: {programName}
+    <div className={classes.Container}>
+      <Link to="..">back</Link>
+      {Object.entries(data).map(([key, value]) => {
+        if (key === "id") return null;
+        return (
+          <p key={`${key}`}>{`${
+            key.includes("name") ? "" : key + ":"
+          } ${value}`}</p>
+        );
+      })}
     </div>
   );
 }
