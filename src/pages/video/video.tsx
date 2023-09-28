4b2b8c02db9d9d816c4e7c498aba8f15ac49dfe9
siyoon <siyoon@friendli.ai>
2023-09-28 12:47:12 +0900

    filter video data

--- src/pages/video/video.tsx
+++ src/pages/video/video.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useMatch } from "react-router-dom";
+
+import video22 from "./video-22.json";
+import video23 from "./video-23.json";
+import Paths from "../../routes/paths";
+
+const { data: data22 } = video22;
+const { data: data23 } = video23;
 
 function VideoPage() {
-  const { name } = useParams();
-  return <div>videoPage param: {name}</div>;
+  const param22Name = useMatch(`${Paths[22].video}/:name`)?.params.name;
+  const param23Name = useMatch(`${Paths[23].video}/:name`)?.params.name;
+
+  const filteredData = (({ is22, is23 }: { is22?: string; is23?: string }) => {
+    if (is22) return data22.find((datum) => datum.name === is22);
+    if (is23) return data23.find((datum) => datum.name === is23);
+  })({ is22: param22Name, is23: param23Name });
+
+  return <div>videoPage param: {filteredData?.name}</div>;
 }
 
 VideoPage.displayName = "VideoPage";
