ad586a6fd60d4eca68288ee70af7f376dc66fa76
siyoon <siyoon@friendli.ai>
2023-10-11 01:27:28 +0900

    refactor wanderobj

--- src/pages/wander/wander-23.tsx
+++ src/pages/wander/wander-23.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import classes from "./wander-22.module.scss";
+import classes from "./wander-23.module.scss";
 import stories from "./wander-data-23";
+import ProgressiveImg from "../../components/utils/progressive-image";
 import WanderOBJ from "../../components/wander-obj/wander-obj";
 import wanderBackground from "../../imgs/wander/2023/wanderBackground.webp";
+import Paths from "../../routes/paths";
 
 function Wander23Page() {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.Container}>
       <img
@@ -13,18 +18,22 @@ function Wander23Page() {
         loading="lazy"
         draggable={false}
         alt="background"
-        className={classes.BackgroundImage_2023}
+        className={classes.BackgroundImage}
       />
       <div className={classes.Story}>
         {stories.map((story) => (
           <WanderOBJ
             key={story.name}
-            imgsrc={story.src}
-            placeholderSrc={story.placeholderSrc}
             translation={story.translation}
             text={story.text}
-            name={story.name}
-          />
+            onSubmit={() => navigate(`${Paths[23].video}/${story.name}`)}
+          >
+            <ProgressiveImg
+              placeholderSrc={story.placeholderSrc}
+              src={story.src}
+              alt={story.name}
+            />
+          </WanderOBJ>
         ))}
       </div>
     </div>
