6afe2caf8648f152aa332e0b3567153afa9eb0b3
leesiyoon <siyoon@friendli.ai>
2023-10-29 15:09:43 +0900

    add 2023 about data

--- src/pages/about/about-23.tsx
+++ src/pages/about/about-23.tsx
@@ -1,14 +1,14 @@
 import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 
-import classes from "./about-22.module.scss";
+import classes from "./about-23.module.scss";
 import contentRaw from "./content-23.json";
 import languagesRaw from "./languages-23.json";
-import type { Content, Languages } from "./model";
+import type { Content2023, Languages } from "./model";
 import poster from "../../imgs/poster/poster-23.webp";
 import LanguageButtons from "../../views/about/language-buttons";
 
-const texts = JSON.parse(JSON.stringify(contentRaw)) as Content;
+const texts = JSON.parse(JSON.stringify(contentRaw)) as Content2023;
 const languages = JSON.parse(JSON.stringify(languagesRaw)) as Languages;
 function About23Page() {
   const [language, setLanguage] = React.useState<string>(
@@ -40,14 +40,54 @@ function About23Page() {
           </motion.h2>
         </AnimatePresence>
         <AnimatePresence>
+          <motion.h3
+            key={`title-${texts[language].subtitle1.slice(0, 5)}`}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.25, duration: 0.25 }}
+            className={classes.SubTitle}
+          >
+            {texts[language].subtitle1}
+          </motion.h3>
+        </AnimatePresence>
+        <AnimatePresence>
           <motion.div
-            key={`title-${texts[language].text}`}
+            key={`title-${texts[language].text1.slice(0, 5)}`}
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.5, duration: 0.25 }}
             className={classes.TextContainer}
           >
-            <p>{texts[language].text}</p>
+            <p>{texts[language].text1}</p>
+          </motion.div>
+        </AnimatePresence>
+        <AnimatePresence>
+          <motion.hr
+            key={`divider-${texts[language].subtitle2.slice(0, 5)}`}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.25, duration: 0.25 }}
+            className={classes.Divider}
+          />
+          <motion.h3
+            key={`title-${texts[language].subtitle2.slice(0, 5)}`}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.25, duration: 0.25 }}
+            className={classes.SubTitle}
+          >
+            {texts[language].subtitle2}
+          </motion.h3>
+        </AnimatePresence>
+        <AnimatePresence>
+          <motion.div
+            key={`title-${texts[language].text2.slice(0, 5)}`}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.25 }}
+            className={classes.TextContainer}
+          >
+            <p>{texts[language].text2}</p>
           </motion.div>
         </AnimatePresence>
       </div>
--- src/pages/about/model.ts
+++ src/pages/about/model.ts
@@ -2,6 +2,17 @@ export interface Content {
   [key: string]: {
     title: string;
     text: string;
+    subtitle?: string;
+  };
+}
+
+export interface Content2023 {
+  [key: string]: {
+    title: string;
+    subtitle1: string;
+    text1: string;
+    subtitle2: string;
+    text2: string;
   };
 }
 
