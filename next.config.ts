cfa3274934bbd0e62cb25b02b100aa9ee0eefad6
leesiyoon <siyoon@friendli.ai>
2025-06-08 14:24:56 +0900

    misc: make commands work

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  /** {@link https://nextjs.org/docs/app/guides/memory-usage#disable-static-analysis} */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    webpackMemoryOptimizations: true,
+    serverSourceMaps: false,
+  },
+  webpack(config, { dev }) {
+    /** {@link https://nextjs.org/docs/app/guides/memory-usage#disable-webpack-cache} */
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: "memory",
+      });
+    }
+  },
 };
 
 export default nextConfig;
