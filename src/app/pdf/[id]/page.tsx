bd810f5843096db50f201d7bfe78a5b688d12d17
siyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2025-12-28 01:20:20 +0900

    fix pdf credit pages

--- src/app/pdf/[id]/page.tsx
+++ src/app/pdf/[id]/page.tsx
@@ -4,7 +4,6 @@ import { notFound } from 'next/navigation';
 import { cn } from '@/shared/cn';
 
 import { PDFPageClient } from './pdf-page.client';
-import data from './data.json';
 
 interface PDFPageProps {
   params: Promise<{ id: string }>;
@@ -35,8 +34,6 @@ export default async function PDFPage({ params }: PDFPageProps) {
     notFound();
   }
 
-  const credit = data.find((item) => item.id === id);
-
   return (
     <div
       className={cn(
@@ -46,16 +43,6 @@ export default async function PDFPage({ params }: PDFPageProps) {
       )}
     >
       <PDFPageClient id={id} />
-      <div
-        className={cn(
-          'relative right-auto bottom-auto',
-          'sm:absolute sm:right-4 sm:bottom-4',
-          'text-sm text-white'
-        )}
-      >
-        <p className="font-semibold whitespace-pre-line">{credit?.title}</p>
-        <p>{credit?.credit}</p>
-      </div>
     </div>
   );
 }
--- src/app/pdf/header.tsx
+++ src/app/pdf/header.tsx
@@ -1,17 +1,36 @@
+'use client';
+
 import Link from 'next/link';
+import { useParams, usePathname } from 'next/navigation';
 
 import { ScaleAdjuster } from '@/widgets/pdf/scale-adjuster.client';
 import { ModeSelector } from '@/widgets/pdf/view-mode-selector.client';
+import { cn } from '@/shared/cn';
 
 export function Header() {
+  const { id } = useParams();
+  const pathname = usePathname();
+  const isInCredit = pathname.includes('credit');
+
   return (
     <div className="sticky top-0 z-10 flex items-center justify-between bg-black px-6 py-5">
       <Link href="/" className="text-sm md:text-base">
         뒤로가기
       </Link>
       <div className="flex items-center gap-2 md:gap-4">
+        <Link
+          href={`${isInCredit ? `/pdf/${id}` : `/pdf/${id}/credit`}`}
+          className={cn(
+            'text-sm hover:underline md:text-base',
+            isInCredit ? 'font-bold underline underline-offset-4' : ''
+          )}
+        >
+          credit
+        </Link>
         <ModeSelector />
-        <ScaleAdjuster />
+        <div className="hidden md:block">
+          <ScaleAdjuster />
+        </div>
       </div>
     </div>
   );
