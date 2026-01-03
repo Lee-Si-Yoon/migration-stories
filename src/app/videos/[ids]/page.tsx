65c273cfefc3da972573a11c8969dd3b941da556
Lee-Si-Yoon <siyoon@friendli.ai>
2026-01-03 17:25:20 +0900

    add multiplayer

--- src/app/videos/[ids]/page.tsx
+++ src/app/videos/[ids]/page.tsx
@@ -1,7 +1,9 @@
+import MultiplayerClient from './multiplayer.client';
+
 export default async function VideosPage({ params }: { params: Promise<{ ids: string }> }) {
   const { ids } = await params;
   const decodedIds = decodeURIComponent(ids);
   const idsArray = decodedIds.split(',');
 
-  return <div>{decodedIds}</div>;
+  return <MultiplayerClient ids={idsArray} />;
 }
